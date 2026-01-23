2a319dd8aed89aca19ca49b4f5c71fea3716f35c
peaky76 <robertjamespeacock@gmail.com>
2026-01-23 17:35:13 +0000

    feat: implement config handling for format state

---
+++ internal/config/config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+	CurrentFormat string `toml:"current_format"`
+}
+
+const configDir = ".musiccat"
+const configFile = "config.toml"
+
+func GetConfigPath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(home, configDir, configFile), nil
+}
+
+func LoadConfig() (*Config, error) {
+	path, err := GetConfigPath()
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return &config, nil
+	}
+
+	_, err = toml.DecodeFile(path, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func SaveConfig(config *Config) error {
+	path, err := GetConfigPath()
+	if err != nil {
+		return err
+	}
+
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return toml.NewEncoder(file).Encode(config)
+}
