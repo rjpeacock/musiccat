cf1988f8375ec2b5e5f4a03a9e7627a15d22f4ee
peaky76 <robertjamespeacock@gmail.com>
2026-01-23 19:08:28 +0000

    feat: add DB insert functions for releases and ownership

--- internal/db/db.go
+++ internal/db/db.go
@@ -66,3 +66,69 @@ func BootstrapDB(db *sql.DB) error {
 	}
 	return nil
 }
+
+func InsertRelease(db *sql.DB, artist, title string, year *int, mbid *string) (int64, error) {
+	query := `INSERT OR IGNORE INTO releases (artist, title, year, musicbrainz_release_group_id) VALUES (?, ?, ?, ?)`
+	args := []interface{}{artist, title}
+	if year != nil {
+		args = append(args, *year)
+	} else {
+		args = append(args, nil)
+	}
+	if mbid != nil {
+		args = append(args, *mbid)
+	} else {
+		args = append(args, nil)
+	}
+	result, err := db.Exec(query, args...)
+	if err != nil {
+		return 0, err
+	}
+	return result.LastInsertId()
+}
+
+func InsertOwnership(db *sql.DB, releaseID int64, formatCategory string, formatDetail *string, purchaseDate *string, cost *float64, source *string, notes *string, discogsID *int) (int64, error) {
+	query := `INSERT INTO ownership (release_id, format_category, format_detail, purchase_date, cost, source, notes, discogs_release_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
+	args := []interface{}{releaseID, formatCategory}
+	if formatDetail != nil {
+		args = append(args, *formatDetail)
+	} else {
+		args = append(args, nil)
+	}
+	if purchaseDate != nil {
+		args = append(args, *purchaseDate)
+	} else {
+		args = append(args, nil)
+	}
+	if cost != nil {
+		args = append(args, *cost)
+	} else {
+		args = append(args, nil)
+	}
+	if source != nil {
+		args = append(args, *source)
+	} else {
+		args = append(args, nil)
+	}
+	if notes != nil {
+		args = append(args, *notes)
+	} else {
+		args = append(args, nil)
+	}
+	if discogsID != nil {
+		args = append(args, *discogsID)
+	} else {
+		args = append(args, nil)
+	}
+	result, err := db.Exec(query, args...)
+	if err != nil {
+		return 0, err
+	}
+	return result.LastInsertId()
+}
+
+func GetReleaseID(db *sql.DB, mbid string) (int64, error) {
+	var id int64
+	err := db.QueryRow("SELECT id FROM releases WHERE musicbrainz_release_group_id = ?", mbid).Scan(&id)
+	return id, err
+}
